99a091a2fa5e3e05f094b2dc66a1638d7940eac2
Shinador <ibofeim2@gmail.com>
2020-04-28 11:27:42 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/Prjava01.java
+++ src/prjava01/Prjava01.java
@@ -1,27 +1,41 @@
 package prjava01;
+
 /**
-*
-* @author Shinador1718daw2vib (a on XYZ és el teu nom d'usuari Github)
-*/
+ *
+ * @author Shinador1718daw2vib (a on XYZ és el teu nom d'usuari Github)
+ */
 import java.io.*;
+
 public class Prjava01 {
-/**
-* @param args the command line arguments
-*/
-public static void main(String[] args) throws IOException {
-File f = new File("fitxer00.html");
-try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
-bw.write("<html>");bw.newLine();
-bw.write(" <head>");bw.newLine();
-bw.write(" <title>");bw.newLine();
-bw.write(" Nova p&agrave;gina web");bw.newLine();
-bw.write(" </title>");bw.newLine();
-bw.write(" </head>");bw.newLine();
-bw.write(" <body>");bw.newLine();
-bw.write(" Nova p&agrave;gina web");bw.newLine();
-bw.write(" </body>");bw.newLine();
-bw.write("</html>");bw.newLine();
-bw.close();
-}
-}
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer00.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");
+            bw.newLine();
+            bw.write(" <head>");
+            bw.newLine();
+            bw.write(" <title>");
+            bw.newLine();
+            bw.write(" Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write(" </title>");
+            bw.newLine();
+            bw.write(" </head>");
+            bw.newLine();
+            bw.write(" <body>");
+            bw.newLine();
+            bw.write("    <h1>DAM2 m05uf2pr4</h1>");bw.newLine();
+            bw.write(" Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write(" </body>");
+            bw.newLine();
+            bw.write("</html>");
+            bw.newLine();
+            bw.close();
+        }
+    }
 }
